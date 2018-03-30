c1dea88c61bf37d33d8a37f7c0b381d1788565f4
Heyman Zhang <heymanzhang@augmentum.com.cn>
2018-03-30 18:10:17 +0800

    add starter-actuator

--- OauthSecurityClient/src/main/java/com/oauth/UiSecurityConfig.java
+++ OauthSecurityClient/src/main/java/com/oauth/UiSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+
 @EnableOAuth2Sso
 public class UiSecurityConfig extends WebSecurityConfigurerAdapter {
 }
